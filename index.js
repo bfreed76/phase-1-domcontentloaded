157a42f46a723601cd9e883eb757e1994c3e8775
Brinton <brintonfoyreed@gmail.com>
2021-04-13 11:48:39 -0700

    commit tree 6d10fefe11a17ae7962aba9ea1e801dce4afb114

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const text = document.getElementById("text");
+    text.innerHTML = "This is really cool!"
+});
+
+console.log("This log fires when index.js loads.");
